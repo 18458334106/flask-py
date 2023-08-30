eb0368ede0eeaa21ce9d12393fdbd3a857b1384a
19857191790 <2418671097@qq.com>
2023-08-30 19:55:57 +0800

    删除注释2

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 from utils.entity import r
-
 from api.user.user import user_bp
 # 创建 Flask 实例
 app = Flask(__name__)
 app.register_blueprint(user_bp)
 app.config['SECRET_KEY'] = 'I LIKE U'
 CORS(app, supports_credentials=True,resources=r'/*')
-
 @app.route('/', methods=['GET'])
 def hello():
     return r(code=200, msg='服务器访问成功!')
