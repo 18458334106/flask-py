0776759c6b4f584b13dcb385b09c32affa2ec879
19857191790 <2418671097@qq.com>
2023-10-08 19:47:42 +0800

    添加用户留言

--- api/user_message/user_message.py
+++ api/user_message/user_message.py
@@ -1,7 +1,7 @@
 from utils.sql import supabase
 from flask import Blueprint,request, session
 from utils.entity import r
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required,get_jwt_identity
 user_message_bp = Blueprint('user_message', __name__, url_prefix='/user_message')
 @user_message_bp.route('/get_messageList', methods=['GET'])
 def getMessageList():
@@ -25,3 +25,44 @@ def getMessageList():
     """
     result = supabase.table('user_message').select('*').execute()
     return r(code=200,msg='获取成功', data=result.data)
+
+@user_message_bp.route('/add_message', methods=['GET'])
+@jwt_required()
+def addMessage():
+    """添加用户留言
+        ---
+        tags:
+          - user_message
+        parameters:
+          - name: Authorization
+            in: header
+            required: true
+            description: 用户token
+            type: string
+          - name: message
+            in: path
+            required: true
+            description: 用户留言
+            type: string
+        responses:
+          200:
+            description: 成功
+            schema:
+              properties:
+                code:
+                  type: integer
+                msg:
+                  type: string
+                data:
+                  type: object
+          401:
+            description: 失败
+    """
+    userInfo = get_jwt_identity()
+    print(userInfo)
+    if not userInfo:
+        return r(msg='暂未登录')
+    else:
+        message = request.args.get('message')
+        result = supabase.table('user_message').insert({"message":message,"username":userInfo["name"]}).execute()
+        return r(code=200,msg='添加成功')
