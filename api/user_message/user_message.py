b3abba9fe1b53a44bd7e28190dc3464498b01559
19857191790 <>
2023-10-08 10:36:45 +0800

    获取消息列表 API文档

--- api/user_message/user_message.py
+++ api/user_message/user_message.py
@@ -6,5 +6,23 @@ user_message_bp = Blueprint('user_message', __name__, url_prefix='/user_message'
 
 @user_message_bp.route('/get_messageList', methods=['GET'])
 def getMessageList():
+    """获取用户留言列表
+        ---
+        tags:
+          - user_message
+        responses:
+          200:
+            description: 成功
+            schema:
+              properties:
+                code:
+                  type: integer
+                msg:
+                  type: string
+                data:
+                  type: object
+          401:
+            description: 失败
+    """
     result = supabase.table('user_message').select('*').execute()
-    return r(msg='获取成功', data=result.data)
+    return r(code=200,msg='获取成功', data=result.data)
