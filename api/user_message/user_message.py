c8a37ec7a9615586d4dbd17d2ab081b495f61af4
19857191790 <>
2023-10-08 16:57:41 +0800

    获取消息列表 API文档 上传文件保存至工作路径

--- api/user_message/user_message.py
+++ api/user_message/user_message.py
@@ -2,6 +2,7 @@ from utils.sql import supabase
 from flask import Blueprint,request, session
 from utils.entity import r
 from flask_jwt_extended import jwt_required
+import os
 user_message_bp = Blueprint('user_message', __name__, url_prefix='/user_message')
 @user_message_bp.route('/get_messageList', methods=['GET'])
 def getMessageList():
@@ -56,6 +57,10 @@ def upload():
     file = request.files['file']
     if file:
         filename = file.filename
-        savePath = 'D:\\' + filename
-        file.save(savePath)
+        # 获取当前工作目录
+        current_dir = os.getcwd()
+        # 拼接文件保存的路径
+        save_path = os.path.join(current_dir, 'uploads', filename)
+        # 保存文件
+        file.save(save_path)
         return r(code=200,msg='上传成功', data={'filename': filename})
