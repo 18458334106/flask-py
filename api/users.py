db7ee9a19a804c8df7ec81317665c8d68b03bca2
18458334106 <2418671097@qq.com>
2024-07-03 13:54:19 +0800

    1111111

--- api/users.py
+++ api/users.py
@@ -3,7 +3,7 @@ from flask import Blueprint,request
 from utils.entity import r
 from flask_jwt_extended import create_access_token, jwt_required ,get_jwt_identity
 from flask_apscheduler import APScheduler
-
+import requests
 users_bp:Blueprint = Blueprint('users', __name__, url_prefix='/users')
 @users_bp.route('/login', methods=['POST'])
 def login():
@@ -179,3 +179,36 @@ def info():
         description: 失败
     """
     return r(msg='获取用户信息成功', data=get_jwt_identity(), code=200)
+
+@users_bp.route('/sendMsg',methods=['GET'])
+def sendMsg():
+    """发送短信验证码
+    ---
+    tags:
+      -  用户
+    consumes:
+      - multipart/form-data
+    parameters:
+      - name: phone
+        in: path,query
+        required: true
+        description: 手机号
+        type: string
+    responses:
+      200:
+        description: 成功
+        schema:
+          properties:
+            code:
+              type: integer
+            msg:
+              type: string
+            data:
+              type: object
+      401:
+        description: 失败
+    """
+    phone = request.args.to_dict().get('phone')
+    resp = requests.get('http://154.12.30.80:90/send.php',params={'phone':phone})
+    print(resp.content)
+    return r(code=200,data=None)
