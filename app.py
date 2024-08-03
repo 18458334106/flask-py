ebfae052c9f80730a12a4bd316500e02d2dab603
19857191790 <2418671097@qq.com>
2024-08-03 22:06:54 +0800

    Revert "剥离其他模块"
    This reverts commit a0372386ec0f2a7cb3bc2a14be327734c9d9b397.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import random
 from flask import Flask
 from flask_cors import CORS
 from utils.entity import r
 from api.users import users_bp
+import requests
+# from api.message.message import user_message_bp
+# from api.examples.examples import examples_bp
 from api.chat import socketio,chat_bp
 from utils.swagger import Swagger
 from flask_jwt_extended import JWTManager
@@ -11,7 +15,9 @@ from utils.sql import supabase
 # 创建 Flask 实例
 app = Flask(__name__)
 app.register_blueprint(users_bp)
+# app.register_blueprint(user_message_bp)
 app.register_blueprint(chat_bp)
+# app.register_blueprint(examples_bp)
 
 CORS(app, supports_credentials=True,resources=r'/*')
 app.config['JWT_SECRET_KEY'] = 'focusInYou' #jwt密钥 可自定义
@@ -52,6 +58,7 @@ def sql_task():
 app.config.from_object(Config())
 
 if __name__ == '__main__':
+    # app.run(debug=True)
     scheduler = APScheduler()
     scheduler.init_app(app)
     scheduler.start()
