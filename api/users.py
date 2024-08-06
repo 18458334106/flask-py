06b01eb2e8680a61149b290d7a210ab7b2f00f4a
赵剑铖 <2418671097@qq.com>
2024-08-06 11:09:53 +0000

    update api/users.py.
    Signed-off-by: 赵剑铖 <2418671097@qq.com>

--- api/users.py
+++ api/users.py
@@ -6,10 +6,6 @@ import aiohttp,base64
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-from io import BytesIO
-from PIL import Image
-from utils.utils import img_simi
-from chaojiying import Chaojiying_Client
 
 users_bp:Blueprint = Blueprint('users', __name__, url_prefix='/users')
 @users_bp.route('/login', methods=['POST'])
@@ -187,56 +183,6 @@ def info():
     """
     return r(msg='获取用户信息成功', data=get_jwt_identity(), code=200)
 
-@users_bp.route('/sendMsg',methods=['GET'])
-async def sendMsg():
-    """发送短信验证码
-    ---
-    tags:
-      -  用户
-    consumes:
-      - multipart/form-data
-    parameters:
-      - name: phone
-        in: path,query
-        required: true
-        description: 手机号
-        type: string
-    responses:
-      200:
-        description: 成功
-        schema:
-          properties:
-            code:
-              type: integer
-            msg:
-              type: string
-            data:
-              type: object
-      401:
-        description: 失败
-    """
-    phone = request.args.to_dict().get('phone')
-    sql = supabase.table('users').select('*').execute()
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=64, verify_ssl=False)) as session:
-        async with session.post('https://webapi.kangruijk.com/admin/sys/sysadmin/sendMsgCode',data={
-            'userPhone': phone
-        }) as resp:
-            async with session.get('http://154.12.30.80:90/randIp.php') as resp1:
-                return r(code=200, data={
-                    'resp': await resp.text(),
-                    'resp1': await resp1.text(),
-                })
-        # async with session.get('http://154.12.30.80:90/send2.php') as resp:
-        #     async with session.post('https://ai.applet.taxplus.cn/Api/sendCode.html',data={'phone':phone}) as resp1:
-        #         async with session.post('https://ai.taxplus.cn/send/register_send.html',data={'phone':phone}) as resp2:
-        #             return r(code=200, data={
-        #                 'resp': await resp.text(),
-        #                 'resp1': await resp1.text(),
-        #                 'resp2': await resp2.text()
-        #             })
-
-
-# pkmmwvplgnmiebgg
 @users_bp.route('/sendEmailMsg',methods=['GET'])
 def sendEmailMsg():
     """发送邮箱验证码
@@ -287,62 +233,3 @@ def sendEmailMsg():
 
     server.sendmail('2418671097@qq.com', email, message.as_string())
     return r(code=200, msg='success', data=None)
-
-@users_bp.route('/sendMsgAuto',methods=['GET'])
-async def sendMsgAuto():
-    """发送短信验证码
-    ---
-    tags:
-      -  用户
-    consumes:
-      - multipart/form-data
-    parameters:
-      - name: phone
-        in: path,query
-        required: true
-        description: 手机号
-        type: string
-    responses:
-      200:
-        description: 成功
-        schema:
-          properties:
-            code:
-              type: integer
-            msg:
-              type: string
-            data:
-              type: object
-      401:
-        description: 失败
-    """
-    phone = request.args.to_dict().get('phone')
-    sql = supabase.table('users').select('*').execute()
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=64, verify_ssl=False)) as session:
-        async with session.get('https://ai.app.taxplus.cn/api/getParams') as resp:
-            data = await resp.json()
-            result = data['data']
-            # 去除base64数据的前缀
-            prefix, data = result['img'].split(',', 1)
-            # 解码base64数据
-            decoded_data = base64.b64decode(data)
-            # 使用BytesIO将字节数据转为文件对象
-            image_file = BytesIO(decoded_data)
-            # 使用PIL创建图片
-            image = Image.open(image_file)
-            bg = image.crop([0, 0, 340, 190])
-            bg.save('bg.png')
-            target = image.crop([4, 196, 56, 246])
-            target.save('target.png')
-            chaojiying = Chaojiying_Client('18458334106', 'Zhaojc2002@', '961726')
-            im = open('bg.png', 'rb').read()
-            re = chaojiying.PostPic(im, 9101)
-            value = int(re['pic_str'].split(',')[0]) - 25
-            async with session.post('https://ai.app.taxplus.cn/Api/sendCode.html',data={
-                'value': value,
-                'key': result['key'],
-                'phone': phone
-            }) as resp1:
-                ddd = await resp1.json()
-                code = ddd['data']['code']
-                return r(code=200,data=code,msg='success')
