2e1fa625378c7eff182304aacb0b648584f360fb
19857191790 <2418671097@qq.com>
2024-09-11 20:12:04 +0800

    1111

--- api/cron_users.py
+++ api/cron_users.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from utils.entity import r
 from utils.sql import supabase
 
-cron_users:Blueprint = Blueprint('chat', __name__ ,url_prefix='/cron_users')
+cron_users:Blueprint = Blueprint('cron_users', __name__ ,url_prefix='/cron_users')
 @cron_users.route('/', methods=['GET'])
 def index():
     response = sql = supabase.table('users').select('*').execute()
